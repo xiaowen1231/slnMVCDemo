8086ffe137b2cc76be22fc0c71af577c61be237e
xiaowen1231 <shiao1124@gmail.com>
2023-06-30 16:37:17 +0800

    加入專案檔案。

---
+++ prjMVCDemo/Controllers/AController.cs
@@ -0,0 +1,78 @@
+﻿using prjMauiDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjMVCDemo.Controllers
+{
+    public class AController : Controller
+    {
+        public string sayHello()
+        {
+            return "Hello ASP.NET MVC.";
+        }
+
+        public string lotto()
+        {
+            return (new CLotto()).getNumber();
+        }
+
+        public string demoResponse()
+        {
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.Filter.Close();
+            Response.WriteFile(@"C:\note\01.jpg");
+            Response.End();
+            return "";
+        }
+        public string demoRequest()
+        {
+            string id = Request.QueryString["id"];
+            if (id == "0")
+                return "蘋果 加入購物車成功";
+
+            else if (id == "1")
+                return "香蕉 加入購物車成功";
+
+            return "找不到該產品資料";
+
+        }
+        public string demoParameter(int? cid)
+        {
+            if (cid == 0)
+                return "蘋果 加入購物車成功";
+
+            else if (cid == 1)
+                return "香蕉 加入購物車成功";
+
+            return "找不到該產品資料";
+
+        }
+
+        public string demoServer()
+        {
+            return "目前伺服器上的實體位置：" + Server.MapPath(".");
+        }
+
+        public string queryById(int? id)
+        {
+            if (id == null)
+            {
+                return "未輸入ID";
+            }
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+            con.Open();
+            con.Close();
+        }
+        // GET: A
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ prjMVCDemo/Models/CLotto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjMauiDemo.Models
+{
+    public class CLotto
+    {
+        public string getNumber()
+        {
+
+            int[] numbers = new int[6];
+
+            Random rnd = new Random();
+
+            int count = 0;
+            
+            while (count < 6)
+            {
+                int temp = rnd.Next(1,50); 
+
+                if (!is在陣列中已有此隨機數(temp,numbers))
+                {
+                    numbers[count] = temp;
+                    count++;
+                }
+                   
+            }
+
+            for (int i = numbers.Length-1; i >0; i--)
+            {
+                for (int j = 0; j < i ; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int big = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j+1] = big;
+                    }
+                }
+            }
+
+            string result = string.Empty;
+
+            foreach(int i in numbers)
+            {
+                result += i+" ";
+            }
+
+            return result;
+        }
+
+        private bool is在陣列中已有此隨機數(int temp, int[] numbers)
+        {
+            foreach(int i in numbers)
+            {
+                if(temp == i)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
