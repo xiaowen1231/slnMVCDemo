c86457acc837b610c06f83e5a01418fb44c36359
xiaowen1231 <shiao1124@gmail.com>
2023-07-05 16:53:20 +0800

    20230705

---
+++ prjMVCDemo/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjMVCDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult List()
+        {
+            string keyword = Request.Form["txtKeyword"];
+            IEnumerable<tProduct> datas = null;
+            dbDemoEntities db = new dbDemoEntities();
+            if (string.IsNullOrEmpty(keyword)) 
+            { 
+                datas = from t in db.tProduct
+                        select t;
+            }
+            else
+            {
+                datas = db.tProduct.Where(p => p.fName.Contains(keyword));
+            }
+            return View(datas);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tProduct p)
+        {
+            var db = new dbDemoEntities();
+            db.tProduct.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if(id!=null)
+            {
+                dbDemoEntities db = new dbDemoEntities();
+                tProduct prod = db.tProduct.FirstOrDefault(p=>p.fId==id);
+                if(prod!=null)
+                {
+                    db.tProduct.Remove(prod);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("List");
+            }
+            dbDemoEntities db = new dbDemoEntities();
+            tProduct prod = db.tProduct.FirstOrDefault(x=>x.fId==id);
+            
+            return View(prod);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tProduct productInForm)
+        {
+            var db = new dbDemoEntities();
+            tProduct productInDb = db.tProduct.FirstOrDefault(x => x.fId == productInForm.fId);
+            if (productInDb!=null)
+            {
+                productInDb.fName = productInForm.fName;
+                productInDb.fQty = productInForm.fQty;
+                productInDb.fCost = productInForm.fCost;
+                productInDb.fPrice = productInForm.fPrice;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ prjMVCDemo/Controllers/ShoppingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjMVCDemo.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        // GET: Shopping
+        public ActionResult List()
+        {
+            var db = new dbDemoEntities();
+            var data = from p in db.tProduct
+                       select p;
+            return View(data);
+        }
+
+        public ActionResult AddToCart(int? id)
+        {
+            ViewBag.fId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart()
+        {
+            return RedirectToAction("List");
+        }
+    }
+}
